dcf10ceddb1baa34ccbd9a5a13c860ec1e92edfd
alt1236 <apt5919@psu.edu>
2026-01-20 11:44:32 -0500

    Added Objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args){
-        Motercycle m1 = new Motercycle();
+        Motercycle m1 = new Motercycle("Red");
+        System.out.println(m1.getcolor());
+
+        Motercycle m2 = new Motercycle("Blue");
+        System.out.println(m2.getcolor());
     }
 }
--- src/Motercycle.java
+++ src/Motercycle.java
@@ -1,3 +1,10 @@
 public class Motercycle {
     private String color;
+
+    public Motercycle(String color){
+        this.color = color;
+    }
+    public String getcolor(){
+        return this.color;
+    }
 }
